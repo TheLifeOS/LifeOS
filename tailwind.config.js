4ba8b3b82ce0fdcd33d4afa8e1c39b33231aa09d
TheLifeOS <amkvk1@gmail.com>
2026-01-11 20:36:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
